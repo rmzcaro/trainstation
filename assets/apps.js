4d661b3bbe2e5b0d873e34bac0de4abb44fd23a8
Carolina RC <caro.rmz315@gmail.com>
2018-03-10 20:25:34 -0600

    get to-do value from textbox and put in var

--- assets/apps.js
+++ assets/apps.js
@@ -6,9 +6,13 @@ $(document).ready(function(){
     //Create an initial count varible
     var addTrain = 0;
 
-    //on click event associated with add-to-do function
-    $("#add-to-do").on("click", function(event){
+    //on click event associated with to-do function
+    //this is the id in HTML
+    $("#add-train").on("click", function(event){
         event.preventDefault();
+
+        //Get the to-do (write-train) value from the text box and store it in a var
+        var writeTrainTask = $("#write-train").val().trim();
     })
 
 });
