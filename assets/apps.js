8314a6d8f9c4fe0af95cc5e465f2782a75886f77
Carolina RC <caro.rmz315@gmail.com>
2018-03-10 20:16:27 -0600

    add variable and on click event

--- assets/apps.js
+++ assets/apps.js
@@ -1,14 +1,14 @@
 // Make input form including button 
-// make divs when new input is created  
-
-
+// make divs when new input is created 
 
 $(document).ready(function(){
-    console.log("ya se armo!"
-    )
+    // console.log("ya se armo")
+    //Create an initial count varible
+    var addTrain = 0;
+
+    //on click event associated with add-to-do function
+    $("#add-to-do").on("click", function(event){
+        event.preventDefault();
+    })
+
 });
-
-
-$(document).ready(function(){
-    console.log("It took me so long but I am here   ")
-})
