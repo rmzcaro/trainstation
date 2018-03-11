a8b1354593bd2ae0a8ba64a8ddf98737651cfd41
Carolina RC <caro.rmz315@gmail.com>
2018-03-10 20:03:18 -0600

    added cdns

---
+++ assets/apps.js
@@ -0,0 +1,14 @@
+// Make input form including button 
+// make divs when new input is created  
+
+
+
+$(document).ready(function(){
+    console.log("ya se armo!"
+    )
+});
+
+
+$(document).ready(function(){
+    console.log("It took me so long but I am here   ")
+})
