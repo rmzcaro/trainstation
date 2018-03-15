c5bd9b4569786db4cbf80d4418eeb4fec0aaa557
Carolina RC <caro.rmz315@gmail.com>
2018-03-15 15:57:17 -0500

    clean push to firebase

--- assets/apps.js
+++ assets/apps.js
@@ -12,32 +12,43 @@ $(document).ready(function(){
     $("#add-train").on("click", function() {
         //don't refresh page
         event.preventDefault();
-        
-        //add 1 to clickCounter
-        // addTrain++;
 
-        //logic for storing and retrieving the most recent user
-        train = $("#train-input").val().trim();
+        //logic for storing and retrieving the most recent user input
+        //grab values from text boxes
+        var train = $("#train-input").val().trim();
+
+            //creat local temporary object for holding train data
+            // click count is created as a key on database, key can be called anything
+            // var newTrain = {
+            //     train:train
+            // };
 
         // refers to root object Firebase 
-        //firebase.database
         database.ref().push({
-            // click count is created as a key on database
-            //key can be called anything
-            // when setting I am telling db 
-            // clickCount: addTrain
-            train:train,
+            train: train
+            //add date: 
+            // dateAdded:firebase.database.ServerValue.TIMESTAMP
         });
 
+        //clears text-boxes
+        //Not sure if needed 
+        $("train-input").val("");
     });
+
         //we are listening to changes in value anywhere in database run this function
-        database.ref().on("value", function(snapshot) {
+        database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
             //callback function
             //if you try to access snap we'll get a promise
             //to get the object we use snapshot.val
             // console.log(snapshot.val());
-            console.log(snapshot.val().train);
+            console.log(childsnapshot.val());
+
+            //store everything into variable
+            var trainName = childSnapshot.val().name;
+
+            // train info 
+            console.log(trainName);
 
             //store info and others can access info
             //change HTML to reflect
@@ -48,4 +59,4 @@ $(document).ready(function(){
             console.log("The read failed: " + errorObject.code);
         });
 
-}); 
+    });
