74e263b38fc39b65e61e03a62d0e2cf0899ae9a6
Carolina RC <caro.rmz315@gmail.com>
2018-03-16 00:24:04 -0500

    adding var train input and freq

--- assets/apps.js
+++ assets/apps.js
@@ -7,6 +7,8 @@ $(document).ready(function(){
     //Initial Values 
     var train = "";
     var destin = "";
+    var ftrain = "";
+    var freq = "";
 
     //on click event associated with to-do function
     //this is the id in HTML
@@ -18,6 +20,9 @@ $(document).ready(function(){
         //grab values from text boxes
         var train = $("#train-input").val().trim();
         var destin= $("#destin-input").val().trim();
+        var ftrain = $("#ftrain-input").val().trim();
+        var freq = $("#freq-input").val().trim();
+        
 
             //creat local temporary object for holding train data
             // click count is created as a key on database, key can be called anything
@@ -28,7 +33,9 @@ $(document).ready(function(){
         // refers to root object Firebase 
         database.ref().push({
             train: train,
-            destin:destin
+            destin: destin,
+            ftrain: ftrain,
+            freq: freq
             //add date: 
             // dateAdded:firebase.database.ServerValue.TIMESTAMP
         });
@@ -36,6 +43,7 @@ $(document).ready(function(){
         //clears text-boxes
         $("#train-input").val("");
         $("#destin-input").val("");
+        $("#ftrain-input").val("");
     });
 
         //we are listening to changes in value anywhere in database run this function
@@ -49,10 +57,16 @@ $(document).ready(function(){
             //to get the object we use snapshot.val
             // console.log(snapshot.val());
             console.log(sv.train);
+            console.log(sv.destin);
+            console.log(sv.ftrain);
 
             //store info and others can access it
             //change HTML to reflect
             $("#train-display").text(sv.train);
+            $("#destin-display").text(sv.destin);
+            $("#traintime-display").text(sv.ftrain); 
+            $("#frequency-display").text(sv.train); 
+            
 
             //catches an error or exception
         }, function(errorObject) {
